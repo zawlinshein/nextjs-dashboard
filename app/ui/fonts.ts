25a456812abe44f783a61382f2b734e1568d2a37
zawlinshein <pudgeismyfavourate@gmail.com>
2024-07-21 21:46:31 +0630

    learning next js

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], style: ['normal', 'normal', 'normal'], })
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400'
+})
+
